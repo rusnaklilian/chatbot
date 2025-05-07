f6da3651b403e79168c38de5650f12a3832a6cb8
rusnaklilian <146160934+rusnaklilian@users.noreply.github.com>
2025-05-07 14:44:43 +0300

    upgrade

--- src/components/ChatForm.jsx
+++ src/components/ChatForm.jsx
@@ -1,28 +1,29 @@
-import {useRef} from 'react';
+import { useRef } from "react";
 
-const ChatForm = ({setChatHistory}) => {
+const ChatForm = ({ chatHistory, setChatHistory, generateBotResponse }) => {
   const inputRef = useRef();
-
   const handleFormSubmit = (e) => {
     e.preventDefault();
     const userMessage = inputRef.current.value.trim();
-    if(!userMessage) return;
-    inputRef.current.value = '';
-
-    // Update chat history with user message
+    if (!userMessage) return;
+    inputRef.current.value = "";
+    // Update chat history with the user's message
     setChatHistory((history) => [...history, { role: "user", text: userMessage }]);
-
-    // Add a "Thinking..." message
-    setTimeout(() => setChatHistory((history) => [...history, { role: "model", text: "Thinking..." }]),
-    600);
+    // Delay 600 ms before showing "Thinking..." and generating response
+    setTimeout(() => {
+      // Add a "Thinking..." placeholder for the bot's response
+      setChatHistory((history) => [...history, { role: "model", text: "Thinking..." }]);
+      // Call the function to generate the bot's response
+      generateBotResponse([...chatHistory, { role: "user", text: `Using the details provided above, please address this query: ${userMessage}` }]);
+    }, 600);
   };
-
   return (
-    <form action="#" className="chat-form" onSubmit={handleFormSubmit}>
-        <input ref={inputRef}    type="text" placeholder="Message..." className="message-input" required />
-        <button className="material-symbols-rounded">arrow_upward</button>
+    <form onSubmit={handleFormSubmit} className="chat-form">
+      <input ref={inputRef} placeholder="Message..." className="message-input" required />
+      <button type="submit" id="send-message" className="material-symbols-rounded">
+        arrow_upward
+      </button>
     </form>
   );
 };
-
 export default ChatForm;
--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -1,11 +1,13 @@
+import ChatbotIcon from "./ChatbotIcon";
+
 const ChatMessage = ({ chat }) => {
-    return (
-        <div className={`message ${chat.role === "model" ? 'bot' : 'user'}-message`}>
-           {chat.role === "model" && <ChatbotIcon />}
-           <p className="message-text">{chat.text}</p>
-        </div>
-    );
-  };
-  
-  export default ChatMessage;
-  
+  return (
+    !chat.hideInChat && (
+      <div className={`message ${chat.role === "model" ? "bot" : "user"}-message ${chat.isError ? "error" : ""}`}>
+        {chat.role === "model" && <ChatbotIcon />}
+        <p className="message-text">{chat.text}</p>
+      </div>
+    )
+  );
+};
+export default ChatMessage;
