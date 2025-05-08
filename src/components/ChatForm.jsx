036f9a24c5bf5f9d5e9dd3eb1338b8ff16d22089
rusnaklilian <146160934+rusnaklilian@users.noreply.github.com>
2025-05-08 20:59:49 +0300

    ChatForm.jsx

--- src/components/ChatForm.jsx
+++ src/components/ChatForm.jsx
@@ -2,21 +2,26 @@ import { useRef } from "react";
 
 const ChatForm = ({ chatHistory, setChatHistory, generateBotResponse }) => {
   const inputRef = useRef();
+
   const handleFormSubmit = (e) => {
     e.preventDefault();
     const userMessage = inputRef.current.value.trim();
     if (!userMessage) return;
     inputRef.current.value = "";
-    // Update chat history with the user's message
+
+    // Добавить сообщение пользователя
     setChatHistory((history) => [...history, { role: "user", text: userMessage }]);
-    // Delay 600 ms before showing "Thinking..." and generating response
+
+    // Задержка и "Thinking..." сообщение
     setTimeout(() => {
-      // Add a "Thinking..." placeholder for the bot's response
       setChatHistory((history) => [...history, { role: "model", text: "Thinking..." }]);
-      // Call the function to generate the bot's response
-      generateBotResponse([...chatHistory, { role: "user", text: `Using the details provided above, please address this query: ${userMessage}` }]);
+      generateBotResponse([
+        ...chatHistory,
+        { role: "user", text: userMessage },
+      ]);
     }, 600);
   };
+
   return (
     <form onSubmit={handleFormSubmit} className="chat-form">
       <input ref={inputRef} placeholder="Message..." className="message-input" required />
@@ -26,4 +31,5 @@ const ChatForm = ({ chatHistory, setChatHistory, generateBotResponse }) => {
     </form>
   );
 };
+
 export default ChatForm;
